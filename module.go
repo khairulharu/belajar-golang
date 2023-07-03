c69b6d82bbd43b2af33165987016cbfde0c2775f
khairulharu <khairulaswad719@gmail.com>
2023-07-03 16:53:11 +0800

    change v1.0.1

--- module.go
+++ module.go
@@ -3,3 +3,11 @@ package belajargolang
 func Module(aType int, bType int) int {
 	return aType * bType
 }
+
+func Attack(zType int, xType int) bool {
+	if zType == xType {
+		return true
+	} else {
+		return false
+	}
+}
