d9e9f8ecd541620ddd75522b363ba9a6525911ef
khairulharu <khairulaswad719@gmail.com>
2023-07-03 19:56:10 +0800

    update v1.1.0

--- module.go
+++ module.go
@@ -1,13 +1,29 @@
 package belajargolang
 
-func Module(aType int, bType int) int {
+func Mltplction(aType int, bType int) int {
 	return aType * bType
 }
 
-func Attack(zType int, xType int) bool {
+func Equal(zType int, xType int) bool {
 	if zType == xType {
 		return true
 	} else {
 		return false
+	}
+}
+
+func Substrac(aType int, bType int) int {
+	return aType - bType
+}
+
+func Addction(aType int, bType int) int {
+	return aType - bType
+}
+
+func Sameas(zType int) string {
+	if zType%2 == 0 {
+		return "genap"
+	} else {
+		return "ganjil"
 	}
 }
